f243c871100bf9b9883e3dcb9fa89fb8b283cc0c
Fredrik Andersson <freand@gmail.com>
2020-05-26 20:08:43 +0200

    * Change protocol to handle bigger data chunks

--- SonosNowPlaying/SonosNowPlaying.ino
+++ SonosNowPlaying/SonosNowPlaying.ino
@@ -11,23 +11,15 @@
 MCUFRIEND_kbv tft;
 WiFiServer server(80);
 
-/* HEADER_DATA pkg_length, row_index, col_offset, col_length */
-#define HEADER_LENGTH (1 + 1 + 1 + 1)
+/* HEADER_DATA pkg_length, x, y, width, height */
+#define HEADER_LENGTH (1 + 1 + 1 + 1 + 1)
+#define BUFFER_V_SIZE 80
 #define BUFFER_H_SIZE 480
 
 
-static uint16_t dataBuffer16[HEADER_LENGTH + BUFFER_H_SIZE];
+static uint16_t dataBuffer16[HEADER_LENGTH + BUFFER_V_SIZE*BUFFER_H_SIZE];
 
 #define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
-#define GREY    0x8410
-#define ORANGE  0xE880
 
 void setup()
 {
@@ -49,8 +41,8 @@ void setup()
     WiFi.begin(ssid, password);
     
     while (WiFi.status() != WL_CONNECTED) {
-      delay(500);
-      Serial.print(".");
+        delay(500);
+        Serial.print(".");
     }
 
     tft.drawRGBBitmap(0, 0, image_data, IMAGE_X_SIZE, IMAGE_Y_SIZE);
@@ -64,62 +56,73 @@ void setup()
     server.begin();
 }
 
+WiFiClient client;
+size_t readPos = 0;
+
 void loop(void)
 {
-  WiFiClient client = server.available();
-  if (client)
+    if (!client)
     {
-      Serial.println("Connected");
-      size_t readPos = 0;
-      uint8_t* dataBuffer8 = (uint8_t*)dataBuffer16;
-      memset(dataBuffer16, 0, sizeof(dataBuffer16));
-      while(client.connected())
+        client = server.available();
+        if (client)
 	{
-	  if (client.available())
+            Serial.println("Connected");
+            readPos = 0;
+            memset(dataBuffer16, 0, sizeof(dataBuffer16));
+	}
+    }
+    else
+    {
+        uint8_t* dataBuffer8 = (uint8_t*)dataBuffer16;
+        if (client.connected())
+	{
+            if (client.available())
 	    {
-	      int readBytes = client.read(&dataBuffer8[readPos], sizeof(dataBuffer16) - readPos);
-	      if (readBytes > 0)
+                int readBytes = client.read(&dataBuffer8[readPos], sizeof(dataBuffer16) - readPos);
+                if (readBytes > 0)
 		{
-		  readPos += readBytes;
-		  
-		  if (dataBuffer16[0] == readPos)
+                    readPos += readBytes;
+                    if (dataBuffer16[0] == readPos)
 		    {
-		      if (dataBuffer16[3] == 0)
+                        if ((dataBuffer16[3] == 0) && (dataBuffer16[4] == 0))
 			{
-			  tft.fillScreen(BLACK);
+                            tft.fillScreen(BLACK);
 			}
-		      else
+                        else
 			{
 			  
-			  tft.drawRGBBitmap(0, dataBuffer16[1], &dataBuffer16[4], dataBuffer16[3], 1);
+                            tft.drawRGBBitmap(dataBuffer16[1], dataBuffer16[2], &dataBuffer16[5], dataBuffer16[3], dataBuffer16[4]);
 			}
-		      readPos = 0;
-		      memset(dataBuffer16, 0, sizeof(dataBuffer16));
-		      client.write("ACK!");
+                        readPos = 0;
+                        memset(dataBuffer16, 0, sizeof(dataBuffer16));
+                        client.write("ACK!");
 		    }
-		  else
+                    else
 		    {
-		      if (readPos == sizeof(dataBuffer16))
+                        if (readPos == sizeof(dataBuffer16))
 			{
-			  Serial.println("Protocol error (full buffer), stop!");
-			  client.stop();
-			  continue;
+                            Serial.println("Protocol error (full buffer), stop!");
+                            Serial.println(readPos);
+                            client.stop();
+                            readPos = 0;
 			}
-		      else if (readPos > dataBuffer16[0])
+                        else if (readPos > dataBuffer16[0])
 			{
-			  Serial.println("Protocol error (pkg error), stop!");
-			  client.stop();
-			  continue;
+                            Serial.println("Protocol error (pkg error), stop!");
+                            Serial.println(dataBuffer16[0]);
+                            Serial.println(readPos);
+                            client.stop();
+                            readPos = 0;
 			}
 		    }
 		}
 	    }
 	}
-      client.stop();
+        else
+	{
+            Serial.println("Connection lost");
+            client.stop();
+            readPos = 0;
+	}
     }
-    else
-    {
-
-    }
-
 }
